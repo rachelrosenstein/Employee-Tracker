ccc3843d503637391b66b2906333b8e69384c043
Rachel Rosenstein <rachelrosenstein1@gmail.com>
2020-11-12 15:10:54 -0500

    started prompt

--- server.js
+++ server.js
@@ -1,3 +1,87 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table")
+
+// connection
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port;
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    password: "23Peaches#",
+    database: "employee_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start();
+});
+
+
+// function to start
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "start",
+                choices: [
+                    "Add Employee",
+                    "View all Employees",
+                    "Remove Employee",
+                    "Add Department",
+                    "View all Departments",
+                    "Add Roles",
+                    "View all Roles",
+                    "Update Employee Role",
+                    "Exit"
+                ]
+            }
+        ])
+        .then(function (res) {
+            switch (res.start) {
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "View all Employees":
+                    viewAllEmployees();
+                    break;
+
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+
+                case "Add Department":
+                    addDept();
+                    break;
+
+                case "View all Departments":
+                    viewAllDept();
+                    break;
+
+                case "Add Roles":
+                    addRole();
+                    break;
+
+                case "View all Roles":
+                    viewAllRoles();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        })
+}
